5a7ce995c9a33f59987fc674f34cf0e2bec17e65
Kitsune254 <owokoselwyn@gmail.com>
2026-05-15 09:36:57 +0300

    adding files

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
