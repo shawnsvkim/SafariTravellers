5aee5334c6c92edc03623d0937eac19b9136b1b2
Kitsune254 <owokoselwyn@gmail.com>
2026-05-17 21:29:31 +0300

    adding imports

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+db = SQLAlchemy(app)
+
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('index.html')
 
